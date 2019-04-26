267a9fd59b43623a56d6e5b7509bdc67bca2771d
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-04-26 09:54:48 -0400

    added package json scripts, webpack config,index.html

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './src',
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
