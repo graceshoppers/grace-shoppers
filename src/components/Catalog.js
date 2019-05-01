04238d4ebfab83e5c257074521a9b9523cd8bebd
Kevin Han <yd.han181@gmail.com>
2019-05-01 19:23:25 -0400

    restructured App, Catalog, and SingleProduct component

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,81 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../styles/Catalog.css';
 import CatalogCard from './CatalogCard';
 
 import {connect} from 'react-redux';
-import {fetchCategories} from '../redux-store/actions/category-actions';
-import {
-  fetchProducts,
-  fetchProductsByCategory,
-  searchForProductsInDatabase,
-} from '../redux-store/actions/product-actions';
 
-class Catalog extends Component {
-  setLocalProps = () => {
-    const {location, match} = this.props;
-    const urlArray = location.pathname.split('/');
+const Catalog = props => {
+  const products = filterProducts(props.products, props.match);
 
-    const filterKeyword = urlArray[urlArray.indexOf('catalog') + 1];
+  return (
+    <div className="overflow-container">
+      <ul className="column-list">
+        {products.length ? (
+          products.map(product => {
+            return <CatalogCard key={product.id} attributes={product} />;
+          })
+        ) : (
+          <CatalogCard
+            key="noProducts"
+            attributes={{id: 0, name: 'No products'}}
+          />
+        )}
+      </ul>
+    </div>
+  );
+};
 
-    this.props.fetchCategories();
-
-    if (!filterKeyword) this.props.fetchProducts();
-    //
-    else if (filterKeyword.toLowerCase() === 'search') {
-      this.props.searchProducts(match.params.searchTerm);
-    }
-    //
-    else if (filterKeyword) {
-      this.props.fetchProductsByCategory(filterKeyword);
-    }
-  };
-
-  componentDidMount() {
-    this.setLocalProps();
-  }
-
-  componentDidUpdate(previousProps) {
-    if (previousProps.location.pathname !== this.props.location.pathname)
-      this.setLocalProps();
-  }
-
-  render() {
-    const {products} = this.props;
-
-    return (
-      <div className="overflow-container">
-        <ul className="column-list">
-          {products.length ? (
-            products.map(product => {
-              return <CatalogCard key={product.id} attributes={product} />;
-            })
-          ) : (
-            <CatalogCard
-              key="noProducts"
-              attributes={{id: 0, name: 'No products'}}
-            />
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+const filterProducts = (productsArray, match) => {
+  if (match.params.searchTerm)
+    return productsArray.filter(product => product.name === 'Trinity Ring');
+  else if (!match.params.category) return productsArray;
+  else return productsArray.filter(product => product.categoryId === 1);
+};
 
 const mapStateToProps = ({products}) => ({products});
 
-const mapDispatchToProps = dispatch => ({
-  fetchProducts: () => dispatch(fetchProducts()),
-
-  fetchProductsByCategory: category =>
-    dispatch(fetchProductsByCategory(category)),
-
-  fetchCategories: () => dispatch(fetchCategories()),
-
-  searchProducts: searchTerm =>
-    dispatch(searchForProductsInDatabase(searchTerm)),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Catalog);
+export default connect(mapStateToProps)(Catalog);
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,58 +1,34 @@
-import React, {Component} from 'react';
-
+import React from 'react';
 import {connect} from 'react-redux';
-import {
-  fetchProductById,
-  deleteProduct,
-} from '../redux-store/actions/product-actions';
 
-class SingleProduct extends Component {
-  componentDidMount() {
-    this.props.fetchProductById(+this.props.match.params.id);
-  }
-  render() {
-    const {products, deleteProduct} = this.props;
-    // const product = products.find(
-    //   product => product.id === +this.props.match.params.id
-    // );
-    // if (!product) return null;
+const SingleProduct = props => {
+  const product = findProduct(props.products, +props.match.params.id);
+  if (!product) return null;
 
-    const product = products[0];
-    if (!product) return null;
+  return (
+    <div>
+      <img src={product.imageName} />
+      <h1>{product.name}</h1>
+      <h3>{product.material}</h3>
+      <p>{product.description}</p>
 
-    return (
-      <div>
-        <img src={product.imageName} />
-        <h1>{product.name}</h1>
-        <h3>{product.material}</h3>
-        <p>{product.description}</p>
+      {/* Delete product button */}
+      <button
+        style={{color: 'red', marginRight: '1em'}}
+        onClick={() => deleteProduct(product.id)}
+      >
+        Delete Product
+      </button>
 
-        {/* Delete product button */}
-        <button
-          style={{color: 'red', marginRight: '1em'}}
-          onClick={() => deleteProduct(product.id)}
-        >
-          Delete Product
-        </button>
+      {/* Add to cart button */}
+      <button>Add to Cart</button>
+    </div>
+  );
+};
 
-        {/* Add to cart button */}
-        <button>Add to Cart</button>
-      </div>
-    );
-  }
-}
+const findProduct = (productsArray, productId) =>
+  productsArray.find(product => product.id === productId);
 
-const mapStateToProps = state => ({
-  products: state.products,
-});
+const mapStateToProps = ({products}) => ({products});
 
-const mapDispatchToProps = dispatch => ({
-  fetchProductById: productId => dispatch(fetchProductById(productId)),
-
-  deleteProduct: productId => dispatch(deleteProduct(productId)),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SingleProduct);
+export default connect(mapStateToProps)(SingleProduct);
