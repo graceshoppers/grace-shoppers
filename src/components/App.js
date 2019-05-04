e6300ae09e128d5e671a3a4f73a9a679bd72e517
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-04 13:02:38 -0400

    resolve conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,8 @@ import Cart from './Cart';
 import SingleProduct from './SingleProduct';
 import Login from './Login';
 import SignUp from './SignUp';
-<<<<<<< HEAD
 import Checkout from './Checkout';
-=======
 import AccountSettings from './AccountSettings';
->>>>>>> 5aa73728fede827a8f5884bb1d5aad697c5753a4
 
 import '../styles/App.css'
 class App extends Component {
