8d41e4a15aba9a15e1d7605a57008d3155faacbd
Kyle Jiang <keeler@gmail.com>
2019-05-04 15:46:53 -0400

    got user name into reviews, added stars ratings

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -4,22 +4,115 @@ import { connect } from "react-redux";
 const Reviews = props => {
   const { reviews } = props;
   const { product } = props;
-  const thisProductReviews = reviews.filter(review => review.productId === product.id)
+  const thisProductReviews = reviews.filter(
+    review => review.productId === product.id
+  );
+
+  const getStars = stars => {
+    if (stars === 1)
+      return (
+        <div>
+          {filledStar()}
+          {emptyStar()}
+          {emptyStar()}
+          {emptyStar()}
+          {emptyStar()}
+        </div>
+      );
+    if (stars === 2)
+      return (
+        <div>
+          {filledStar()}
+          {filledStar()}
+          {emptyStar()}
+          {emptyStar()}
+          {emptyStar()}
+        </div>
+      );
+    if (stars === 3)
+      return (
+        <div>
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+          {emptyStar()}
+          {emptyStar()}
+        </div>
+      );
+    if (stars === 4)
+      return (
+        <div>
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+          {emptyStar()}
+        </div>
+      );
+    if (stars === 5)
+      return (
+        <div>
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+          {filledStar()}
+        </div>
+      );
+  };
+
+  const filledStar = () => {
+    return <img width="25 px" height="25 px" src="/stars/star_filled.png" />;
+  };
+  const emptyStar = () => {
+    return <img width="25 px" height="25 px" src="/stars/star_empty.png" />;
+  };
 
   return (
     <div>
-    <h2>REVIEWS BEGIN HERE</h2>
-    <span>CUSTOMER NAME PLACEHOLDER</span>
-     {thisProductReviews.map(thisProductReview => {
+      <h4>What our customers say...</h4>
+
+      {thisProductReviews.map(thisProductReview => {
+        const {
+          id,
+          stars,
+          title,
+          createdAt,
+          textBody,
+          recommended
+        } = thisProductReview;
+        const { firstName, lastName } = thisProductReview.user;
+
         return (
-            <div>
-            {thisProductReview.stars}
-        <p>{thisProductReview.textBody}</p>
-        </div>
-        )
-    })}
-      
-    </div> 
+          <div  key={id}>
+            <div className="row">
+              <div className="col">
+                <h5>{title}</h5>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-auto">
+                {firstName}&nbsp;
+                {lastName}
+              </div>
+              <div className="col">{createdAt.slice(0,10)}</div>
+            </div>
+            <div className="row">
+              <div className="col-2">{getStars(stars)}</div>
+            </div>
+            <div className="row">
+              <div className="col"><p>{textBody}</p></div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <h6>Recommended: {recommended ? "Yes" : "No"}</h6>
+              </div>
+            </div>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
