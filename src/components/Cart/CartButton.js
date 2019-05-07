91b0b432587961026e74aa71e6e5d0158ab852bc
Kevin Han <yd.han181@gmail.com>
2019-05-07 12:37:58 -0400

    Feature: add badge indicating number of cart items on cart button.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {NavLink} from 'react-router-dom';
 
-const CartButton = () => {
+const CartButton = props => {
+  //   const {numberOfCartItems} = props;
+
   return (
     <NavLink to="/cart" className="btn">
       <i className="fas fa-shopping-cart fa-lg" />
+
+      <span className="badge badge-pill badge-primary">
+        {/* {numberOfCartItems} */}
+        99
+      </span>
     </NavLink>
   );
 };
+
+// CartButton.propTypes = {
+//   numberOfCartItems: PropTypes.number.isRequired || PropTypes.string.isRequired,
+// };
 
 export default CartButton;
