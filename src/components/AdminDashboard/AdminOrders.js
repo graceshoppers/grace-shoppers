c032a36e86e5dc92ea4f9a7d62fe779a74e6739b
Kyle Jiang <keeler@gmail.com>
2019-05-12 00:39:54 -0400

    added some basic div styling to admin orders

--- src/components/AdminDashboard/AdminOrders.js
+++ src/components/AdminDashboard/AdminOrders.js
@@ -14,32 +14,57 @@ import {
 
 const AdminOrders = props => {
   const { orders, orderitems } = props;
-  return(
-    <div>{displayOrders(orders, orderitems)}</div>
-  )
- }
 
+  console.log(orders[1])
+  console.log(orderitems[1])
+  return <div>{displayOrders(orders, orderitems)}</div>;
+};
 
-
-const displayOrders = (orders, orderitems) =>{
-  console.log(orderitems)
-    return (orders.map(order => {
-      return (
-        <div key={order.id} style={{border:"1px solid black", marginBottom:"40px"}}>
-          <h3>{order.status}  </h3>
-          <h4>{order.createdAt}</h4>
-          {orderitems
-            .filter(orderitem => order.id === orderitem.orderId)
-            .map(orderitem => {
-              return (
-                <div key={orderitem.id} style={{borderLeft:"solid 5px red", marginBottom:"10px"}} >
-                  <img width="50px" height="50px" src={orderitem.product.imageName}/>{orderitem.product.name}x{orderitem.quantity}
-                </div>
-              );
-            })}
+const displayOrders = (orders, orderitems) => {
+  return orders.map(order => {
+    const {firstName, lastName} = order.user
+    return (
+      <div
+        className="table"
+        key={order.id}
+        style={{ border: "1px solid black", marginBottom: "40px" }}
+      >
+        <div className="row">
+          <div className="col"><h3>{order.status}</h3></div>
+          <div className="col"> 
+              <button style={{width:'35px'}} type="submit" className="btn btn-dark">
+                +
+              </button>
+            </div>
         </div>
-      );
-    }))
+        <div className="row">
+          <div className="col">Ordered on: {order.createdAt.slice(11,16)} {order.createdAt.slice(0,10)}</div>
+        </div>
+        <div className="row">
+          <div className="col">Ordered by: {firstName} {lastName}</div>
+        </div>
+
+    
+        {orderitems
+          .filter(orderitem => order.id === orderitem.orderId)
+          .map(orderitem => {
+            return (
+              <div
+                key={orderitem.id}
+                style={{ borderLeft: "solid 5px black", marginBottom: "10px" }}
+              >
+                <img
+                  width="50px"
+                  height="50px"
+                  src={orderitem.product.imageName}
+                />
+                {orderitem.product.name}x{orderitem.quantity}
+              </div>
+            );
+          })}
+      </div>
+    );
+  });
 };
 
 const mapStateToProps = state => {
