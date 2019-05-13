651be2a896d602b0597958ce47e49625851f058c
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-13 11:43:16 -0400

    changed home style a bit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,24 +8,28 @@ const Home = props => {
     <div className="row home-page d-flex flex-column justify-content-center align-self-center">
       <div
         style={{
-          paddingBottom: '-150px',
-          marginBottom: '-200px',
+          paddingBottom: '-50px',
+          marginBottom: '-100px',
           display: 'inline-block',
         }}
       >
         <div className="horizontal-flip">K</div>BMK
       </div>
+      <h1>Fine Jewelry</h1>
       <img
         src="line.jpg"
         style={{
           height: '200px',
           width: '600px',
           display: 'inline-block',
+          marginTop: '-70px',
           marginBottom: '-50px',
         }}
       />
-      <h3>Fine Jewelry</h3>
-      <NavLink to="/catalog/rings" style={{padding:'0px',margin:'0px',fontSize:'20px'}}>
+      <NavLink
+        to="/catalog/rings"
+        style={{padding: '0px', margin: '0px', fontSize: '20px'}}
+      >
         <button className="collection-link">Explore Our Collections</button>
       </NavLink>
     </div>
