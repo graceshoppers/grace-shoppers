7cf23cad2c3d9e803e4da38e2dc88057071a0f57
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-14 15:44:58 -0400

    checkout button

--- src/components/Cart/CartCheckout.js
+++ src/components/Cart/CartCheckout.js
@@ -6,29 +6,17 @@ const CartCheckout = props => {
   const {userDetails} = props;
 
   return (
-    <div className="col-md-4">
-      <div className="container">
-        <div className="row align-items-start">
-          <div className="col-md-12">
-            <h3 className="shopping-cart-title">Checkout</h3>
-            <div className="black-divider-thin" style={{margin: '2em 0'}} />
-          </div>
-          <div className="row justify-content-md-center">
-            <div className="col">
-              <button
-                className="btn btn-success"
-                type="submit"
-                onClick={() => {
-                  props.history.push('/checkout');
-                }}
-              >
-                {' '}
-                Checkout
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <button
+        type="submit"
+        onClick={() => {
+          props.history.push('/checkout');
+        }}
+        style={{fontSize:'30px'}}
+      >
+        {' '}
+        Checkout
+      </button>
     </div>
   );
 };
